ee9c5dacf3cbcc74c66d1abccd5470c336d1a00a
Zeeshnhmd <zeeshnhmd.1@gmail.com>
2024-05-18 15:45:04 +0530

    06-06,07 cart screen, remove from cart

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -16,12 +16,14 @@ import store from './store';
 import './assets/styles/index.css';
 import './assets/styles/bootstrap.min.css';
 import ProductScreen from './screens/ProductScreen';
+import CartScreen from './screens/CartScreen';
 
 const router = createBrowserRouter(
 	createRoutesFromElements(
 		<Route path="/" element={<App />}>
 			<Route index={true} path="/" element={<HomeScreen />} />
 			<Route path="/product/:id" element={<ProductScreen />} />
+			<Route path="/cart" element={<CartScreen />} />
 		</Route>
 	)
 );
